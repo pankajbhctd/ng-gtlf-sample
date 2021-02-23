79bed1ade195c43fd755b2d80f042a72228ee29f
pankajbhctd <pankajbhctd@gmail.com>
2021-02-23 17:40:01 +0530

    abros

--- src/app/gltf/gltf.component.ts
+++ src/app/gltf/gltf.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-gltf',
   templateUrl: './gltf.component.html',
-  styleUrls: ['./gltf.component.scss']
+  styleUrls: ['./gltf.component.scss'],
 })
 export class GltfComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
